8dbdc0dd5170669ff918aae38f06ae63b438b518
deepleela <leelazero@protonmail.com>
2018-04-27 17:55:49 +0800

    using es5 as target

--- build/main/lib/Protocol.js
+++ build/main/lib/Protocol.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Protocol {
-}
-Protocol.sys = {
-    requestAI: 'requestAI',
-    enterRoom: 'enterRoom',
-};
+var Protocol = /** @class */ (function () {
+    function Protocol() {
+    }
+    Protocol.sys = {
+        requestAI: 'requestAI',
+        enterRoom: 'enterRoom',
+    };
+    return Protocol;
+}());
 exports.Protocol = Protocol;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUHJvdG9jb2wuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvbGliL1Byb3RvY29sLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBTUE7O0FBRW9CLFlBQUcsR0FBRztJQUNsQixTQUFTLEVBQUUsV0FBVztJQUN0QixTQUFTLEVBQUUsV0FBVztDQUN6QixDQUFBO0FBTEwsNEJBT0MifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiUHJvdG9jb2wuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvbGliL1Byb3RvY29sLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBTUE7SUFBQTtJQU9BLENBQUM7SUFMbUIsWUFBRyxHQUFHO1FBQ2xCLFNBQVMsRUFBRSxXQUFXO1FBQ3RCLFNBQVMsRUFBRSxXQUFXO0tBQ3pCLENBQUE7SUFFTCxlQUFDO0NBQUEsQUFQRCxJQU9DO0FBUFksNEJBQVEifQ==
